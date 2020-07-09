72dc978351fb6de4bfc30ef50e8342581316cc8d
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-09 05:44:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,5 @@
 class Song 
 @@count = 0 
 
-  def initialize(count)
-  @@count += 1 
-  end 
-
-  def count
-    return @@count
-  end 
-  
-  def genres(genres)
-  @genres = genres
-end 
-
-  def genres 
-    @genres 
-  end 
-  
-  def artists(artists)
-    @artists = artists
-  end 
-  
-  def artists 
-    @artists 
-  end 
   
 end 
